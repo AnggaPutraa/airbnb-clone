3d321834da86dd963c9ea4e817c02e30a19d85a9
AnggaPutraa <yananggaa@gmail.com>
2023-01-06 12:45:24 +0700

    fix: configure plugins on tailwind config

--- components/LiveAnywhereSection.tsx
+++ components/LiveAnywhereSection.tsx
@@ -1,17 +1,25 @@
 import Image from "next/image";
 import { LiveAnyWhere } from "../types/live-anywhere-conent";
+import MediumCard from "./MediumCard";
 
-const LiveAnyWhereSection = ({content}: Props) => {
+const LiveAnyWhereSection = ({ content }: Props) => {
     return (
         <section className="pt-6">
             <h2 className='text-2xl font-semibold pb-5'>Live Anywhere</h2>
-            <div>
-                
+            <div className="flex space-x-4 overflow-scroll scrollbar-hide p-3 -ml-3">
+                {content?.map(
+                    (item, index) => (
+                        <MediumCard
+                            key={index}
+                            content={item}
+                        />
+                    )
+                )}
             </div>
         </section>
     );
 }
- 
+
 export default LiveAnyWhereSection;
 
 interface Props {
