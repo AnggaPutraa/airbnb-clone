7d6e07d23d1767b47e9bb0df534dc3ac62ddf1fe
AnggaPutraa <yananggaa@gmail.com>
2023-01-06 21:50:06 +0700

    refactor: add date range picker

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,29 @@
 import Image from 'next/image'
 import { DynamicHeroIcon } from '../public/icons/DynamicHeroIcon';
+import { DateRangePicker } from 'react-date-range';
+import { ChangeEvent, useState } from 'react';
+import { useRouter } from 'next/router';
+
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 
 const Header = () => {
+    const [searchInput, setSearchInput] = useState('');
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const [guestsCount, setGuestCount] = useState(1);
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection'
+    }
+
+    const handleSelect = (ranges: Range) => {
+        setStartDate(ranges.selection.startDate)
+        setEndDate(ranges.selection.endDate)
+    }
+
     return (
         <header className='sticky top-0 z-20 grid grid-cols-3 bg-white shadow-md p-4 md:px-10'>
             <div className='relative h-8 flex items-center cursor-pointer my-auto'>
@@ -24,6 +46,8 @@ const Header = () => {
             </div>
             <div className='flex items-center border rounded-full md:shadow-sm py-1'>
                 <input
+                    value={searchInput}
+                    onChange={(e) => { setSearchInput(e.target.value) }}
                     type="text"
                     className='pl-5 bg-transparent outline-none w-full placeholder-gray-400' placeholder='Start youre search'
                 />
@@ -49,8 +73,46 @@ const Header = () => {
                     />
                 </div>
             </div>
+            {searchInput && (
+                <div className='flex flex-col col-span-3 mx-auto mt-4'>
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                        minDate={new Date()}
+                        rangeColors={['#FD5861']}
+                        onChange={handleSelect}
+                    />
+                    <div className='flex items-center border-b'>
+                        <h2 className='text-2xl pl-2 flex-grow font-semibold'>Number of Guest</h2>
+                        <DynamicHeroIcon
+                            icon='UsersIcon'
+                            className='h-6 cursor-pointer'
+                        />
+                        <input
+                            min={1}
+                            value={guestsCount}
+                            onChange={(e) => setGuestCount(Number(e.target.value))}
+                            type="number"
+                            className='w-12 pl-2 text-lg outline-none text-red-400'
+                        />
+                    </div>
+                    <div className='flex items-center justify-around mt-2'>
+                        <button
+                            className='text-gray-500'
+                            onClick={() => { setSearchInput('') }}
+                        >Cancel</button>
+                        <button className='text-red-400'>Search</button>
+                    </div>
+                </div>
+            )}
         </header>
     );
 }
 
 export default Header;
+
+interface Range {
+    selection: {
+        startDate: Date
+        endDate: Date
+    }
+}
